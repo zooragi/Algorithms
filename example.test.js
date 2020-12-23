dd3f123aa83330ccde22b82ee129f9152cafcc2d
joe <eldehd9898@gmail.com>
2020-12-23 10:43:26 +0900

    Solve issu#2 reverse prime

--- example.test.js
+++ example.test.js
@@ -1,5 +1,5 @@
-const add =(a,b) => a+b;
+const prime = require("./lectureProblem/daehee/reversPrime/reverseprime.js")
 
 test("1 is 1", () => {
-    expect(add(5,6)).toBe(11);
-  })
+  expect(prime.isPrime(71,15,32)).toBe("71 32")
+})
